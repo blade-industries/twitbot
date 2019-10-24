efffaa9d00ac6b19e095a156cb08b3ba9f93daf1
Daniel Otaigbe <dotaigbe@icloud.com>
2019-10-24 13:10:41 -0400

    Added a Todo to the top of the bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,4 @@
+//TODO: gaming tweets with video, jumbled tweets, 
 // Our Twitter library
 var Twit = require('twit');
 
@@ -5,7 +6,7 @@ var Twit = require('twit');
 var T = new Twit(require('./config.js'));
 
 // This is the URL of a search for the latest tweets on the '#mediaarts' hashtag.
-var mediaArtsSearch = {q: "#gaming", count: 10, result_type: "recent"}; 
+var mediaArtsSearch = {q: "#gaming", count: 10, result_type: "recent"};
 
 // This function finds the latest tweet with the #mediaarts hashtag, and retweets it.
 function retweetLatest() {
